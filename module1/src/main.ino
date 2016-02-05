189b36bbcf573eee6d5f4481fc84e5f9cd808a3e
chebukin <chebukin@mail.ru>
2016-02-05 15:12:03 +0200

    small code

--- module1/src/main.ino
+++ module1/src/main.ino
@@ -1,10 +1,34 @@
-long buttonPressedAt=millis();
-long timeout=300;
-int currentScreen=0;
-int screensCount=5;
+#include "Arduino.h"
+#include <DHT.h>
 
-if (buttonPressed && currentTime-buttonPressedAt>timeout){
-buttonPressedAt = currentTime;
-screen=screen+1;
+int buttonPin=10;
 
+long buttonPressedAt;
+long buttonPressTimeout = 300;
+long sleepTimeout = 30000;
+byte currentScreen = 0;
+byte screensCount = 5;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  buttonPressedAt = millis();
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  boolean buttonPressed = true;
+  buttonPressed = digitalRead(buttonPin);
+  if (buttonPressed && millis() - buttonPressedAt > buttonPressTimeout) {
+    buttonPressedAt = millis();
+    currentScreen = currentScreen + 1;
+    if (currentScreen > screensCount)
+      currentScreen = 0;
+      Serial.println("Button pressed - show screen "+String(currentScreen));
+  }
+  if (millis() - buttonPressedAt > sleepTimeout) {
+    Serial.println("Time is out - display OFF");
+  }
 }
